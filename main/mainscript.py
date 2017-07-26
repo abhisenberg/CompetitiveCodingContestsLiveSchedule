6cc433eae0c3103fad9aadafc78a8f2c7374e520
Abhishek <ab96sh@gmail.com>
2017-07-26 12:22:42 +0530

    Update mainscript.py

--- main/mainscript.py
+++ main/mainscript.py
@@ -24,7 +24,7 @@ def crawl(url):
 
 def crawl_hackerrank():
 
-    driver_1 = webdriver.Firefox(executable_path='/app/geckodriver/geckodriver')
+    driver_1 = webdriver.Firefox(executable_path='/app/geckodriver/')
 
     # if not os.path.isfile(HR_file_path):
     # if os.stat(HR_file_path).st_size == 0:
@@ -84,7 +84,7 @@ def crawl_hackerrank():
 
 
 def crawl_hackerearth():
-    driver_2 = webdriver.Firefox(executable_path='/app/geckodriver/geckodriver')
+    driver_2 = webdriver.Firefox(executable_path='/app/geckodriver/')
     # if not os.path.isfile(HE_file_path):
     # if os.stat(HE_file_path).st_size == 0:
     #     print "Downloading page source of Hackerearth..."
@@ -170,7 +170,7 @@ def crawl_hackerearth():
 
 
 def crawl_codechef():
-    driver_3 = webdriver.Firefox(executable_path='/app/geckodriver/geckodriver')
+    driver_3 = webdriver.Firefox(executable_path='/app/geckodriver/')
     # if not os.path.isfile(CC_file_path):
     # if os.stat(CC_file_path).st_size == 0:
     #     print "Downloading page source of Codechef"
